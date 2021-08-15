89c2a4db1dabad64c5f3c24cbe272e0c5fd7682a
Uriel Nunez <uanq22@hotmail.com>
2021-08-15 17:04:14 -0500

    modified server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3004;
 
 const app = express();
 
